8161d63af3e1295bdd02de128de00fcf0cc32ca4
Sabína Gregušová <sabina.gregusova@gmail.com>
2020-11-05 17:00:43 +0100

    Working on automated conversion

--- Part_2/convertData.py
+++ Part_2/convertData.py
@@ -11,46 +11,21 @@ class convertData:
     Creates connection to MongoDB and MySQL database
     '''
     def __init__(self):
+        self.connectMongo()
         self.connectSQL()
-        print("Connected")
 
     def connectMongo(self):
         self.mongo = pymongo.MongoClient("mongodb://localhost:27017/")
-        self.db = self.mongo["corona"]
-        pass
-
-    ### from stack overflow 
-    # https://stackoverflow.com/questions/19472922/reading-external-sql-script-in-python
-    def executeScriptsFromFile(self, filename):
-        mycursor = self.nosql.cursor()
-        # Open and read the file as a single buffer
-        fd = open(filename, 'r')
-        sqlFile = fd.read()
-        fd.close()
-
-        # all SQL commands (split on ';')
-        sqlCommands = sqlFile.split(';')
-        sqlCommands = [command.strip() for command in sqlCommands if command]
-        print(sqlCommands)
-
-        # Execute every command from the input file
-        for command in sqlCommands:
-            # This will skip and report errors
-            # For example, if the tables do not yet exist, this will skip over
-            # the DROP TABLE commands
-            try:
-                mycursor.execute(command)
-            except OperationalError:
-                print("Command skipped")
+        self.mongodb = self.mongo["corona"]
 
     def connectSQL(self):
-        self.nosql = mysql.connector.connect(
+        self.mysql = mysql.connector.connect(
             host="localhost",
             user="root",
             password="ahojahojahoj"
         )
 
-        self.executeScriptsFromFile('RelationalDatabase.sql')
+        self.mysqlCursor = self.mysql.cursor()
         #mycursor = self.nosql.cursor()
         #with open('RelationalDatabase.sql') as f:
             #mycursor = self.nosql.cursor()
@@ -62,12 +37,32 @@ class convertData:
         #for x in mycursor:
         #    print(x)
 
+    ### from stack overflow 
+    # https://stackoverflow.com/questions/19472922/reading-external-sql-script-in-python
+    def createRelationalDatabase(self, filename):
+        mycursor = self.mysql.cursor()
+        # Open and read the file as a single buffer
+        fd = open(filename, 'r')
+        sqlFile = fd.read()
+        fd.close()
+
+        # all SQL commands (split on ';')
+        sqlCommands = sqlFile.split(';')
+        sqlCommands = [command.strip() for command in sqlCommands if command]
+
+        # Execute every command from the input file
+        for command in sqlCommands:
+            # This will skip and report errors
+            # For example, if the tables do not yet exist, this will skip over
+            # the DROP TABLE commands
+            try:
+                mycursor.execute(command)
+            except OperationalError:
+                print("Command skipped")
+
     '''
     Creates tables for relational databases
     '''
-    def createRelationalDatabase(self):
-        pass
-
     def convertB(self):
         self.collectionB = self.db["B"]
         for x in self.collectionB.find():
@@ -80,7 +75,45 @@ class convertData:
             print(x)
             break
 
+    def fetchCollection(self, collectionName):
+        return self.mongodb[collectionName]
+
+    def fetchCollectionData(self, collectionName):
+        return self.mongodb[collectionName].find()
+
+    def obtainImportantData(self, dataset, labels):
+        val = []
+        for data in dataset:
+            important_data = []
+            for label in labels:
+                important_data.append(data[label])
+            val.append(tuple(important_data))
+            
+        return val
+
+
+    def convertCountries(self):
+        countries = self.fetchCollectionData('countries')
+        vals = self.obtainImportantData(countries, ['alpha2', 'name'])
+        
+        sql_command = "INSERT INTO country_codes (country_code, country_name) VALUES (%s, %s)"
+
+        self.mysqlCursor.executemany(sql_command, vals)
+
+        self.mysql.commit()
+
+        print(self.mysqlCursor.rowcount, " was inserted")
+        #val = []
+        #for data in countries:
+        #    country_name = data['name']
+        #    country_code = data['alpha2'].upper()
+        #    val.append((country_code, country_name))
+
+        #print(val)
+
 
 Convertor = convertData()
+Convertor.createRelationalDatabase('Corona.sql')
+Convertor.convertCountries()
 #Convertor.convertB()
 #Convertor.convertA()
--- Part_2/prepData.sh
+++ Part_2/prepData.sh
@@ -3,6 +3,11 @@
 # clear the database space
 mongo prepNoSQL.js
 
+# prepare the country dataset
+#cat countries.json | sed 's/,$//' > country_code.json
+#sed 's/]$//' |  sed '1s/^[//'
+
+
 # preprare the A dataset
 curl https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/osoby.min.json --output A
 python3 formatJson.py A | sed "s/\bFalse\b/'False'/g" | sed "s/\bTrue\b/'True'/g" > A.json
@@ -24,3 +29,9 @@ curl https://opendata.ecdc.europa.eu/covid19/testing/json/ --output C.json
 mongoimport -d corona -c C --legacy --file C.json --jsonArray
 
 rm -rf C.json
+
+# prepare country code dataset
+mongoimport -d corona -c countries --legacy --file countries.json --jsonArray
+
+# prepare the nuts and lau dataset
+mongoimport -d corona -c nuts_lau --legacy --file nuts_lau.json --jsonArray
